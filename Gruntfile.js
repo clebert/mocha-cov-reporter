6552030ecb3cbcfab64a713299e4cfd1bbe89765
Clemens Akens <clebert@me.com>
2014-01-29 13:42:03 +0100

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,12 +56,12 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('publish', function (type) {
-        grunt.task.run('lint');
+        grunt.task.run('default');
         grunt.task.run('module:check-repository');
         grunt.task.run('bumpup:' + (type || 'patch'));
         grunt.task.run('module:license-copyright');
         grunt.task.run('module:release-publish');
     });
 
-    grunt.registerTask('travis', 'lint');
+    grunt.registerTask('travis', 'default');
 };
