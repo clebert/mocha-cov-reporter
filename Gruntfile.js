9d9439d591bdf7af121efeb15940f81bd14b4f0a
Clemens Akens <clebert@me.com>
2014-10-26 12:41:32 +0100

    Add JSCS and update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,22 @@
 'use strict';
 
+var time = require('time-grunt');
+
 module.exports = function (grunt) {
+    time(grunt);
+
     grunt.initConfig({
         bumpup: {
             file: 'package.json'
+        },
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            src: [
+                '**/*.js',
+                '!node_modules/**/*.js'
+            ]
         },
         jshint: {
             options: {
@@ -40,17 +53,23 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-bumpup');
+    grunt.loadNpmTasks('grunt-jscs');
     grunt.loadNpmTasks('grunt-module');
 
-    grunt.registerTask('default', 'jshint');
+    grunt.registerTask('build', [
+        'jscs',
+        'jshint'
+    ]);
 
     grunt.registerTask('publish', function (type) {
-        grunt.task.run('default');
+        grunt.task.run('build');
         grunt.task.run('module:check-repository');
         grunt.task.run('bumpup:' + type);
         grunt.task.run('module:license-copyright');
         grunt.task.run('module:release-publish');
     });
 
-    grunt.registerTask('travis', 'default');
+    grunt.registerTask('travis', 'build');
+
+    grunt.registerTask('default', 'build');
 };
